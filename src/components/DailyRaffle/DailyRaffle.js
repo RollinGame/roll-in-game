835fdbf357501b81e55bf04f235f66590d3ae818
Evlogi Aleksandrov <evlogi.aleksandrov@gmail.com>
2020-04-19 21:17:26 +0300

    Added Daily raffle leaderbord

--- src/components/DailyRaffle/DailyRaffle.js
+++ src/components/DailyRaffle/DailyRaffle.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import RaffleCTA from './RaffleCTA'
 import Jackpot from './Jackpot'
+import DashboardList from '../LastGames/DashboardList'
+import { RaffleWinners } from '../../data/LastGames'
 
 const DailyRaffle = () => {
+  const [listData, setListData] = useState([])
+
+  useEffect(() => {
+    setListData(RaffleWinners.slice(0, 10))
+  }, [])
+
   return (
     <div class='raffle-container'>
       <RaffleCTA />
       <Jackpot />
+      <DashboardList listData={listData}>
+        <h2 class='dashboard__title'>Latest Winners</h2>
+      </DashboardList>
     </div>
   )
 }
