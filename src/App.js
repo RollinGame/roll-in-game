4b0707d344baf92490ba115adf050e0115264696
Evlogi Aleksandrov <evlogi.aleksandrov@gmail.com>
2020-04-19 10:07:15 +0300

    Added daily raffle cta

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from 'react'
 
-import Carousel from './components/Carousel/Carousel';
-import LastGamesBoard from './components//LastGames/LastGamesBoard';
-
+import Carousel from './components/Carousel/Carousel'
+import LastGamesBoard from './components//LastGames/LastGamesBoard'
+import DailyRaffle from './components/DailyRaffle/DailyRaffle'
 
 const App = () => {
-
   return (
     <div>
       <Carousel />
       <LastGamesBoard />
+      <DailyRaffle />
     </div>
   )
 }
---
+++ src/components/DailyRaffle/DailyRaffle.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import RaffleCTA from './RaffleCTA'
+
+const DailyRaffle = () => {
+  return (
+    <div class='raffle-container'>
+      <RaffleCTA />
+    </div>
+  )
+}
+
+export default DailyRaffle
