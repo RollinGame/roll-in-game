8aff6a80767460d0bb2f0e1ae4600c35a7eaa67f
Evlogi Aleksandrov <evlogi.aleksandrov@gmail.com>
2020-04-19 16:47:05 +0300

    Added Jackpot banner

--- src/components/DailyRaffle/DailyRaffle.js
+++ src/components/DailyRaffle/DailyRaffle.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import RaffleCTA from './RaffleCTA'
+import Jackpot from './Jackpot'
 
 const DailyRaffle = () => {
   return (
     <div class='raffle-container'>
       <RaffleCTA />
+      <Jackpot />
     </div>
   )
 }
