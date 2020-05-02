9c9a87ba49fb05f1bced6c8e298a08005ad8b945
Evlogi Aleksandrov <evlogi.aleksandrov@gmail.com>
2020-05-02 21:10:28 +0300

    dummy

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
-import LandingPage from './LandingPage'
-import RollinDice from './games/RollinDice/RollinDice'
+import LandingPage from './LandingPage';
+import RollinDice from './games/RollinDice/RollinDice';
 
 const App = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route path='/' exact>
-          <LandingPage />
-        </Route>
-        <Route path='/dice'>
-          <RollinDice />
-        </Route>
-      </Switch>
-    </Router>
-  )
-}
+	return (
+		<Router>
+			<Switch>
+				<Route path="/" exact>
+					<LandingPage />
+				</Route>
+				<Route path="/dice">
+					<RollinDice />
+				</Route>
+			</Switch>
+		</Router>
+	);
+};
 
-export { App as default }
+export { App as default };
