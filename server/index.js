c454fd03ef6bcb0a8f499170967b31fc8b63458f
Evlogi Aleksandrov <evlogi.aleksandrov@gmail.com>
2020-05-03 12:04:52 +0300

    Added change streams to testLogItem model

--- server/index.js
+++ server/index.js
@@ -1,3 +1,15 @@
-const runApi = require('./server/runApi');
+const app = require('express')();
+const env = process.env.NODE_ENV || 'development';
+const settings = require('./config/settings')[env];
+require('./config/database')(settings);
+require('./config/express')(app);
+require('./config/routes')(app);
+const testLogs = require('./model/TestLogItem');
 
-runApi();
+const expressServer = app.listen(settings.apiPort);
+console.log(`Server listening on port ${settings.apiPort}...`);
+
+
+const logsEventEmitter = testLogs.watch();
+
+logsEventEmitter.on('change', change => console.log(change.fullDocument));
